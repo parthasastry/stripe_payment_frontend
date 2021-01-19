c54a76470cb94a197e733ee13982189ddf203ce8
parthasastry <pssastry@a483e77984df.ant.amazon.com>
2021-01-18 20:12:47 -0500

    first commit;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StripeCheckout from 'react-stripe-checkout';
 
 function App() {
+
+  const [product, setProduct] = useState({
+    name: "React from FB",
+    price: 10,
+    productBy: 'Facebook'
+  });
+
+  const makePayment = token => {
+    const body = {
+      token,
+      product
+    }
+
+    const headers = {
+      "Content-Type": "application/json"
+    }
+
+    //This has to be https when deployed
+    return fetch(`http://localhost:8282/payment`, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(body)
+    })
+    .then(response => {
+      console.log(response);
+      const { status } = response;
+      console.log("Status: ", status);
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Payment</h1>
+      <StripeCheckout 
+        stripeKey="pk_test_51IB7huDaD5goVVmvi9YCfHydaCnUIY2l0FC6mKFaLFgfLlNWNghIGzAqk3NeFfzKfCNY3fPo5VLSmz04t001qHuu00EMvYSYsU"
+        token={makePayment}
+        billingAddress
+        name="Please donate"
+        amount={product.price * 1000}
+      >
+          <button className="btn-large red">Donate</button>
+      </StripeCheckout>
     </div>
   );
 }
